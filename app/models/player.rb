0df608ce8744813c0b35243295b3b8939811f6e7
Michael Malet <michael@nervd.com>
2014-05-06 11:23:39 +0100

    Add presence constraint to Player#name

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,5 @@
 class Player < ActiveRecord::Base
   has_and_belongs_to_many :games
   has_many :frames
+  validates_presence_of :name
 end
--- spec/models/player_spec.rb
+++ spec/models/player_spec.rb
@@ -4,5 +4,6 @@ describe Player do
   describe 'validations' do
     it{ should have_and_belong_to_many(:games) }
     it{ should have_many(:frames) }
+    it{ should validate_presence_of(:name) }
   end
 end
