90aa7f672dde9e1c01e974c774da255581ee055a
Michael Malet <michael@nervd.com>
2014-05-06 11:51:05 +0100

    Implement basic Game#start! which initialises the frames and checks there are enough players

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,4 +1,21 @@
 class Game < ActiveRecord::Base
   has_and_belongs_to_many :players
   has_many :frames
+
+  FRAMES_PER_GAME = 10
+
+  def start!
+    raise 'You need 1 or more players to start bowling' unless self.players.count > 0
+
+    create_frames
+  end
+
+private
+  
+  def create_frames
+    self.players.each do |player|
+      FRAMES_PER_GAME.times{ self.frames << Frame.create!(game: self, player: player) }
+    end
+  end
+
 end
--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -5,4 +5,31 @@ describe Game do
     it{ should have_many(:frames) }
     it{ should have_and_belong_to_many(:players) }
   end
+
+  describe '#start!' do
+    let(:game){ Game.create!(players: players) }
+    subject{ game.start! }
+    
+    context 'has no players' do
+      let(:players){ [] }
+      it{ expect{ subject }.to raise_error(RuntimeError, "You need 1 or more players to start bowling") }
+    end
+
+    context 'has 1 player' do
+      let(:players){ [Player.create!(name: 'Alice')] }
+      it 'should create 10 frames for 1 player' do
+        subject
+        expect(Frame.where(game: game).count).to eq(10)
+      end
+    end
+
+    context 'has 2 players' do
+      let(:players){ [Player.create!(name: 'Alice'), Player.create!(name: 'Bob')] }
+      it 'should create 20 frames for 2 players' do
+        subject
+        expect(Frame.where(game: game).count).to eq(20)
+      end
+    end
+
+  end
 end
