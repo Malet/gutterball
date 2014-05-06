465b1ef8037273b70e6786f8b6a77743135c60d0
Michael Malet <michael@nervd.com>
2014-05-06 11:19:06 +0100

    Implementation of Frame#strike?

--- app/models/frame.rb
+++ app/models/frame.rb
@@ -2,4 +2,9 @@ class Frame < ActiveRecord::Base
   belongs_to :game
   belongs_to :player
   has_many :rolls
+
+  def strike?
+    1  == self.rolls.count &&
+    10 == self.rolls.first.pins
+  end
 end
--- spec/models/frame_spec.rb
+++ spec/models/frame_spec.rb
@@ -5,4 +5,43 @@ describe Frame do
     it{ should belong_to(:game) }
     it{ should have_many(:rolls) }
   end
+
+  describe '#strike?' do
+    subject{ frame.strike? }
+    let(:frame){ Frame.create!(rolls: rolls) }
+    
+    context 'strike' do
+      let(:rolls){ rolled [10] }
+      it{ should == true }
+    end
+    
+    context 'spare' do
+      let(:rolls){ rolled [5,5] }
+      it{ should == false }
+    end
+
+    context 'nothing has occurred' do
+      context 'no rolls' do
+        let(:rolls){ [] }
+        it{ should == false }
+      end
+
+      context 'no spares or strikes' do
+        let(:rolls){ rolled [2,3] }
+        it{ should == false }
+      end
+
+      context 'gutterballs' do
+        let(:rolls){ rolled [0,0] }
+        it{ should == false }
+      end
+    end
+  end
+
+private
+
+  def rolled(pins_arr)
+    pins_arr.map{ |pins| Roll.create!(pins: pins) }
+  end
+
 end
